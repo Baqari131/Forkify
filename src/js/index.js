08f42c8833829849453137ee667b862f057d1230
Bakari Darsadze <baqardarsadze13@gmail.com>
2024-02-15 08:26:51 +0400

    Search items

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,38 @@
 // Global app controller
+
+import Search from "./modules/search";
+import { clearLoader, elements, renderLoader } from "./views/base";
+import * as searchView from "./views/searchView";
+
+/* 
+- Search object
+- Current recipe object
+- Shopping list object
+- Liked recipies
+*/
+
+const state = {};
+
+const controlSearch = async (e) =>{
+    e.preventDefault();
+
+    const query = searchView.getInput();
+
+    
+    if(query){
+        searchView.clearInput();
+        searchView.clearResults();
+        renderLoader(elements.searchResList);
+        state.search = new Search(query);
+        await state.search.getResults();
+        /* console.log(state); */
+
+        searchView.renderResult(state.search.result);
+    }
+    clearLoader();
+
+    
+}
+
+
+elements.searchForm.addEventListener("submit",controlSearch);
